61846fc741ebbaf7065fe3a926a86b2f0a035d8d
Shubham <master.transcode@gmail.com>
2024-05-01 17:08:22 +0530

    test

--- app/components/NavSocialMedia.tsx
+++ app/components/NavSocialMedia.tsx
@@ -6,10 +6,12 @@ import { FaBehanceSquare, FaFacebookSquare, FaInstagramSquare } from "react-icon
 const NavSocialMedia = () => {
     return (
         <div className="hidden sm:flex text-white p-4 items-center justify-between h-[50px] bg-blue">
-            <div className="container mx-auto text-white py-0.5 flex items-center justify-between ">              
+            <div className="container mx-auto text-white py-0.5 flex items-center justify-between ">
+                <div className="text-white">
                     <Link href="mailto:company@example.com">
-                        <span className="text-white hover:underline" >Email : info@transcodesolution.com</span>
-                    </Link>               
+                        <span className="hover:underline" >Email : info@transcodesolution.com</span>
+                    </Link>
+                </div>
                 <div className="flex space-x-4">
                     <Link href="https://www.linkedin.com/in/transcodesolution/" target="_blank" rel="noopener noreferrer">
                         <span className="text-xl hover:text-white">
